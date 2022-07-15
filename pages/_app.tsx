c76fa71244f233e406d8345d25b472ba20ea1d4c
Maz Hermon <mazhermon@gmail.com>
2022-07-15 14:51:46 +1200

    setup linting and formatting cont

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,10 @@ import { Layout } from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-  );
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
