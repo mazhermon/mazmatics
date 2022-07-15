4b7d6bb6b234fdc43510829f59f529471d408fdb
Maz Hermon <mazhermon@gmail.com>
2022-07-15 14:51:20 +1200

    setup linting and formatting

---
+++ components/layout.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from './navbar'
+import { Footer } from './footer'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,3 @@
+export const Navbar = () => {
+  return <div>hello navbar</div>
+}
