b1ad17416376f20d1e731bcd48c45126049facae
Maz Hermon <mazhermon@gmail.com>
2022-07-15 20:25:30 +1200

    homepage signup styling

---
+++ components/characters/circleBetty.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './circleBetty.module.css'
+
+export const CharCircleBetty = () => (
+  <div className={styles.charCircleBetty}></div>
+)
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+import styles from './navbar.module.css'
+
 export const Navbar = () => {
-  return <div>hello navbar</div>
+  return (
+    <div className={styles.navbar}>
+      <Link href="/">
+        <a className={styles.logo}>Mazmatics</a>
+      </Link>
+      <span className={styles.tagline}>for home-play </span>
+    </div>
+  )
 }
