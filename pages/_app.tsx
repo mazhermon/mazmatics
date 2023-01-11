a02e6607efc9b36495e0d7aa4a7413cffbf41750
Maz Hermon <mazhermon@gmail.com>
2023-01-11 16:23:31 +1300

    fix GA env bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
       />
           
       <Script
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', ${process.env.GA_MEASUREMENT_ID}, {
+          gtag('config', "${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}", {
             page_path: window.location.pathname,
           });
         `,
