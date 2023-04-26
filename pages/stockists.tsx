7418da71ce51c4ad5b09f7d56f2d6c49721d26b9
Maz Hermon <mazhermon@gmail.com>
2023-04-26 16:47:24 +1200

    style: stockists and wholesale

--- pages/stockists.tsx
+++ pages/stockists.tsx
@@ -17,87 +17,110 @@ const Stockists = () => (
         checking on Amazon for your area of the world. International Bookshops
         can now order Fun Math 4 Kids via Ingram Spark.
       </p>
-      <p className={styles.nzLocations}>Mazmatics is proud to be available at the following NZ locations:</p>
+      <p className={styles.nzLocations}>
+        Mazmatics is proud to be available at the following NZ locations:
+      </p>
+
+      <p>
+        Jump to:
+        <ul className={styles.jumpToList}>
+          <li>
+            <a href="#aucklandAnchor">Auckland</a>
+          </li>
+          <li>
+            <a href="#wellingtonAnchor">Wellington</a>
+          </li>
+          <li>
+            <a href="#southIslandAnchor">South Island</a>
+          </li>
+          <li>
+            <a href="#bookstoreBuyerAnchor">Bookstore Buyers</a>
+          </li>
+        </ul>
+      </p>
+
       <div>
-      <h2>Auckland</h2>
-      <ul className={styles.stockists__list}>
-        <li>
-          <BookStore
-            name="The Dorothy Butler Children's Bookshop"
-            location="1 Jervois Road, Ponsonby, Auckland 1011"
-            phone="09 376 7283"
-            website="dorothybutler.co.nz"
-            url="https://dorothybutler.co.nz/p/fun-math-4-for-kids-good-foundations-volume-1?barcode=9780473648916"
-          ></BookStore>
-        </li>
-      </ul>
+        <h2 id="aucklandAnchor">Auckland</h2>
+        <ul className={styles.stockists__list}>
+          <li>
+            <BookStore
+              name="The Dorothy Butler Children's Bookshop"
+              location="1 Jervois Road, Ponsonby, Auckland 1011"
+              phone="09 376 7283"
+              website="dorothybutler.co.nz"
+              url="https://dorothybutler.co.nz/p/fun-math-4-for-kids-good-foundations-volume-1?barcode=9780473648916"
+            ></BookStore>
+          </li>
+        </ul>
 
-      <h2>Wellington</h2>
-      <ul className={styles.stockists__list}>
-        <li>
-          <BookStore
-            name="Schrödinger's Books"
-            location="137 Jackson Street, Petone, Lower Hutt 5012"
-            phone="04 260 3777"
-            website="schrodingersbooks.co.nz"
-            url="https://schrodingersbooks.co.nz/p/fun-math-for-kids-good-foundations?barcode=9780473648916&selected_category=82407"
-          ></BookStore>
-        </li>
-        <li>
-          <BookStore
-            name="The Children's Bookshop"
-            location="Shop 26, Kilbirnie Plaza, Kilbirnie, Wellington 6022"
-            phone="+64 4 387 3905"
-            website="thechildrensbookshop.co.nz"
-            url="https://www.thechildrensbookshop.co.nz/p/fun-math-for-kids-volume-1-good-foundations?barcode=9780473648916"
-          ></BookStore>
-        </li>
-        <li>
-          <BookStore
-            name="Unity Books"
-            location="57 Willis Street, Wellington Central, Wellington 6011"
-            phone="04 499 4245"
-            website="unitybooksonline.co.nz"
-            url="http://unitybooksonline.co.nz/"
-          ></BookStore>
-        </li>
-      </ul>
+        <h2 id="wellingtonAnchor">Wellington</h2>
+        <ul className={styles.stockists__list}>
+          <li>
+            <BookStore
+              name="Schrödinger's Books"
+              location="137 Jackson Street, Petone, Lower Hutt 5012"
+              phone="04 260 3777"
+              website="schrodingersbooks.co.nz"
+              url="https://schrodingersbooks.co.nz/p/fun-math-for-kids-good-foundations?barcode=9780473648916&selected_category=82407"
+            ></BookStore>
+          </li>
+          <li>
+            <BookStore
+              name="The Children's Bookshop"
+              location="Shop 26, Kilbirnie Plaza, Kilbirnie, Wellington 6022"
+              phone="+64 4 387 3905"
+              website="thechildrensbookshop.co.nz"
+              url="https://www.thechildrensbookshop.co.nz/p/fun-math-for-kids-volume-1-good-foundations?barcode=9780473648916"
+            ></BookStore>
+          </li>
+          <li>
+            <BookStore
+              name="Unity Books"
+              location="57 Willis Street, Wellington Central, Wellington 6011"
+              phone="04 499 4245"
+              website="unitybooksonline.co.nz"
+              url="http://unitybooksonline.co.nz/"
+            ></BookStore>
+          </li>
+        </ul>
 
-      <h2>South Island</h2>
-      <ul className={styles.stockists__list}>
-        <li>
-          <BookStore
-            name="Scorpio Books & Telling Tales"
-            location="BNZ Centre - 120 Hereford Street Christchurch 8011"
-            phone="03 3778 462"
-            website="scorpiobooks.co.nz"
-            url="https://scorpiobooks.co.nz/product/fun-math-4-kids-with-mazmatics-vol-1/"
-          ></BookStore>
-        </li>
-        <li>
-          <BookStore
-            name="Timaru Booksellers"
-            location="331 Stafford Street, Timaru, NZ 7910"
-            phone="03 6831 387"
-            website="bayhillbooks.co.nz"
-            url="https://bayhillbooks.co.nz/"
-          ></BookStore>
-        </li>
-        <li>
-          <BookStore
-            name="The Twizel Bookshop"
-            location="inside the Hydro Cafe, Twizel"
-            phone="03 6831 387"
-            website="bayhillbooks.co.nz"
-            url="https://bayhillbooks.co.nz/"
-          ></BookStore>
-        </li>
-      </ul>
-
+        <h2 id="southIslandAnchor">South Island</h2>
+        <ul className={styles.stockists__list}>
+          <li>
+            <BookStore
+              name="Scorpio Books & Telling Tales"
+              location="BNZ Centre - 120 Hereford Street Christchurch 8011"
+              phone="03 3778 462"
+              website="scorpiobooks.co.nz"
+              url="https://scorpiobooks.co.nz/product/fun-math-4-kids-with-mazmatics-vol-1/"
+            ></BookStore>
+          </li>
+          <li>
+            <BookStore
+              name="Timaru Booksellers"
+              location="331 Stafford Street, Timaru, NZ 7910"
+              phone="03 6831 387"
+              website="bayhillbooks.co.nz"
+              url="https://bayhillbooks.co.nz/"
+            ></BookStore>
+          </li>
+          <li>
+            <BookStore
+              name="The Twizel Bookshop"
+              location="inside the Hydro Cafe, Twizel"
+              phone="03 6831 387"
+              website="bayhillbooks.co.nz"
+              url="https://bayhillbooks.co.nz/"
+            ></BookStore>
+          </li>
+        </ul>
       </div>
-      
+      <h2 id="bookstoreBuyerAnchor">Are you a bookstore buyer?</h2>
+      <p className={styles.wholeSaleInfoLink}>
+        If you want to stock Mazmatics in your bookshop, check out our{' '}
+        <Link href="/wholesalers">Wholesale info</Link>
+      </p>
     </div>
-
   </div>
 )
 
--- pages/wholesalers.tsx
+++ pages/wholesalers.tsx
@@ -1,55 +1,73 @@
+import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import styles from './wholesalers.module.css'
+import { Container } from '../components/Container'
+
+const DynamicSunSprite = dynamic(
+  () => import('../components/characters/sunSprite'),
+  {
+    ssr: false,
+  }
+)
 
 const Wholesalers = () => (
-  <div className={`${styles.wholesalers} copyArea`}>
-    <h1>Wholesalers info</h1>
-    <p>
-      Mazmatics Fun Math for Kids is available for your book store. We support
-      small businesses by offering initial consignment terms with wholesale
-      discounts, as well as bulk ordering options for wholesale purchase.
-    </p>
-
-    <h2>Wholesale discounts</h2>
-
-    <table className={styles.wholesaleTable}>
-      <caption className="sr-only">Wholesale Prices</caption>
-      <tbody>
-        <tr>
-          <th scope="row">RRP</th>
-          <td>$24.00</td>
-        </tr>
-        <tr>
-          <th scope="row">Purchase price</th>
-          <td>$11.99 +GST</td>
-        </tr>
-        <tr>
-          <th scope="row">Consignment price</th>
-          <td>$13.50 +GST</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <div className="moreInfo">
+  <Container>
+    <DynamicSunSprite />
+    <div className={`${styles.wholesalers} copyArea`}>
+      <h1>
+        <span className="hyphenated">Wholesalers</span> info
+      </h1>
       <p>
-        Please contact us for more information on minimum orders and consignment
-        terms at{' '}
-        <a
-          className={styles.contactEmailLink}
-          href="mailto:hello@mazmatics.com?subject=Hi Mazmatics, Wholesaler enquiry"
-        >
-          hello@mazmatics.com
-        </a>{' '}
+        Mazmatics Fun Math for Kids is available for your book store. We support
+        small businesses by offering initial consignment terms with wholesale
+        discounts, as well as bulk ordering options for wholesale purchase.
       </p>
       <p>
-        <Link href="/">
-          <a aria-label="home" className={styles.backLink}>
-            &lt; Back
-          </a>
-        </Link>
+        Schools are also welcome to email to make arrangements for bulk purchase
+        options for fundraising initiatives.
       </p>
+
+      <h2>Wholesale discounts</h2>
+
+      <table className={styles.wholesaleTable}>
+        <caption className="sr-only">Wholesale Prices</caption>
+        <tbody>
+          <tr>
+            <th scope="row">RRP</th>
+            <td>$24.00</td>
+          </tr>
+          <tr>
+            <th scope="row">Purchase price</th>
+            <td>$11.99 +GST</td>
+          </tr>
+          <tr>
+            <th scope="row">Consignment price</th>
+            <td>$13.50 +GST</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="moreInfo">
+        <p>
+          Please contact us for more information on minimum orders and
+          consignment terms at{' '}
+          <a
+            className={styles.contactEmailLink}
+            href="mailto:hello@mazmatics.com?subject=Hi Mazmatics, Wholesaler enquiry"
+          >
+            hello@mazmatics.com
+          </a>{' '}
+        </p>
+        <p>
+          <Link href="/">
+            <a aria-label="home" className={styles.backLink}>
+              &lt; Back
+            </a>
+          </Link>
+        </p>
+      </div>
     </div>
-  </div>
+  </Container>
 )
 
 export default Wholesalers
