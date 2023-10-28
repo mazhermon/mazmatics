fb037a1a8fd6f70c4c3d2646f35405977634a8f3
Maz Hermon <mazhermon@gmail.com>
2023-10-29 12:43:10 +1300

    add tracking to download link

--- components/freeSample/index.tsx
+++ components/freeSample/index.tsx
@@ -3,17 +3,31 @@ import React from 'react'
 import styles from './freeSample.module.css'
 import { Button } from '../button'
 
-export const FreeSampleDownload = () => (
-  <>
-    <div className={styles.freeSampleText}>
-      <p>Try before you buy with this free sampler digital edition</p>
-    </div>
-    <Button
-      className={styles.homeFreeSampleCTAbutton}
-      variant="secondary"
-      href="/downloads/Mazmatics_FunMathForKids_vol1_Free_Sample_PDF.pdf"
-    >
-      Download a free sample
-    </Button>
-  </>
-)
+export const FreeSampleDownload = () => {
+  const onFreeSampleDownloadAnalytics = () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    gtag('event', 'free_sample_downloaded', {
+      event_category: 'free_sample_downloaded',
+      event_label: 'free_sample_downloaded',
+    })
+    debugger
+    window.location.href =
+      '/downloads/Mazmatics_FunMathForKids_vol1_Free_Sample_PDF.pdf'
+  }
+  return (
+    <>
+      <div className={styles.freeSampleText}>
+        <p>Try before you buy with this free sampler digital edition</p>
+      </div>
+      <Button
+        onClick={onFreeSampleDownloadAnalytics}
+        className={styles.homeFreeSampleCTAbutton}
+        variant="secondary"
+        // href="/downloads/Mazmatics_FunMathForKids_vol1_Free_Sample_PDF.pdf"
+      >
+        Download a free sample
+      </Button>
+    </>
+  )
+}
